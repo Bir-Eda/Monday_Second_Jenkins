e1e5c4193062296701afa2a5559a70f3e7016437
Bir-Eda <60522150+Bir-Eda@users.noreply.github.com>
2020-07-27 20:19:22 -0400

    finally

---
+++ Monday_Monday/src/module-info.java
@@ -0,0 +1,2 @@
+module Monday_Monday {
+}
---
+++ Monday_Monday/src/mondaypackage/Hello_Class.java
@@ -0,0 +1,11 @@
+package mondaypackage;
+
+public class Hello_Class {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world, eclipse and jenkins");
+		System.out.println("Hello second project");
+	
+	}
+
+}
